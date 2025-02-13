fcbd2ec10a7a0661925af5e804da452659de88cd
h3fects <herrerogarciajl@gmail.com>
2025-02-13 16:49:11 +0100

    diablo

--- Back/Backend-Usuario-App/src/main/java/com/example/demo/repositories/RegistroRepository.java
+++ Back/Backend-Usuario-App/src/main/java/com/example/demo/repositories/RegistroRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.RegistroPractica;
-
+@Repository
 public interface RegistroRepository extends JpaRepository<RegistroPractica, Long>{
 	
 
