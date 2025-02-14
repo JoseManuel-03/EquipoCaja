a07051c8721d63835d197b1702b27475d4f5d6d6
JoseManuel <jordonez5404@ceuandalucia.es>
2025-02-14 16:34:56 +0100

    Back v3

--- Back/Backend-Usuario-App/src/main/java/com/example/demo/service/UserService.java
+++ Back/Backend-Usuario-App/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.RegistroPractica;
@@ -15,7 +16,10 @@ public interface UserService {
 	public void cambiarContraseña(Long getID, String getOldPassword, @Size(min = 8) String newPassword)
 			throws UserUnauthorizedException, UserNotFoundException;
 
-	public List<RegistroPractica> consultarDetalles(Long idUser) throws UserUnauthorizedException, UserNotFoundException;
+	public List<RegistroPractica> consultarDetalles(Long idUser, LocalDate fechaInicio, LocalDate fechaFin)
+			throws UserUnauthorizedException, UserNotFoundException;
 
-	
+	public void crearRegistro(RegistroPractica registroPractica) throws UserUnauthorizedException;
+
+	public void borrarRegistro(Long id) throws UserUnauthorizedException;
 }
