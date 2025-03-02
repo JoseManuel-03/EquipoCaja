720ad1b76d6827adda15b19c5105d872b6bd0d89
h3fects <151049468+h3fects@users.noreply.github.com>
2025-03-02 19:52:12 +0100

    Update UsuarioDTO.java

--- Back/Backend-Usuario-App/src/main/java/com/example/demo/api/request/UsuarioDTO.java
+++ Back/Backend-Usuario-App/src/main/java/com/example/demo/api/request/UsuarioDTO.java
@@ -9,34 +9,28 @@ import lombok.Data;
 
 @Data
 public class UsuarioDTO {
-	private Long id;
-	private String nombreCompleto;
-	private String tutorDocente;
-	private String ciclo;
-	private Integer año;
-	private String evaluacion;
-	private String docente;
-	private String empresaAsignada;
-	private Double cantidadHoras;
+	public class UsuarioDTO {
 
-	public UsuarioDTO(Usuario user, List<RegistroPractica> list) {
-		this.id = user.getId();
-		this.nombreCompleto = (user.getUsuarioAsociado() != null) ? user.getUsuarioAsociado().getNombreCompleto()
-				: "Sin asignar";
-		this.tutorDocente = (user.getUsuarioAsociado() != null && user.getUsuarioAsociado().getTutorDocente() != null)
-				? user.getUsuarioAsociado().getTutorDocente().getNombreCompleto()
-				: "Sin asignar";
-		this.ciclo = (user.getUsuarioAsociado() != null) ? user.getUsuarioAsociado().getCiclo() : "Sin asignar";
+    private Long id;
+    private String nombreUsuario;
+    private String contraseña;
+    private String perfil;
+    private Alumno usuarioAsociado;
+    private Boolean activo;
+    private Double cantidadHoras;
 
-		this.año = user.getUsuarioAsociado().getAnioCurso();
-		this.evaluacion = user.getUsuarioAsociado().getEvaluacion();
-		this.empresaAsignada = (user.getUsuarioAsociado() != null && user.getUsuarioAsociado().getEmpresa() != null)
-				? user.getUsuarioAsociado().getEmpresa().getNombreEmpresa()
-				: "Sin asignar";
-		this.cantidadHoras = (list != null)
-				? list.stream().mapToDouble(r -> (r.getCantidadHoras() != null) ? r.getCantidadHoras() : 0).sum()
-				: 0;
+    public UsuarioDTO(Usuario user, List<RegistroPractica> list) {
+        this.id = user.getId();
+        this.nombreUsuario = user.getNombreUsuario();
+        this.contraseña = user.getContraseña();
+        this.perfil = user.getPerfil();
+        this.usuarioAsociado = user.getUsuarioAsociado();
+        this.activo = user.getActivo();
 
-	}
+        this.cantidadHoras = (list != null)
+                ? list.stream().mapToDouble(r -> (r.getCantidadHoras() != null) ? r.getCantidadHoras() : 0).sum()
+                : 0;
+
+    }
 
 }
